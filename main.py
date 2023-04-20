3cd856bd84765ad78c6de1f1976efac660a276b8
Allerito <allera.ruben.04@gmail.com>
2023-04-20 22:24:27 +0200

    Select chars function added
    divided code in function and added a select chars function

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
 import random
 import string
 
-password = ""
-all_char = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+def get_chars()-> string:
+    """get the all alphabets that the user select
 
-pass_length = int(input("Insert the length of your password: "))
-for i in range(pass_length):
-    password += random.choice(all_char)
+    :return: all alphabet selected
+    :rtype: string
+    """
+    all_char = ""
+    count = 1
+    num = int(input("How many alphabet did u need? (lower, upper, number and special cases)"))
+    while count <= num:
+        select_alphabet=input("Which alphabet do you want? (lower, upper, number or special)")
+        if select_alphabet == "lower" or select_alphabet == "Lower" or select_alphabet == "LOWER" or select_alphabet == "l" or select_alphabet == "L":
+            all_char += string.ascii_lowercase
+        elif select_alphabet == "upper" or select_alphabet == "Upper" or select_alphabet == "UPPER" or select_alphabet == "u" or select_alphabet == "U":
+            all_char += string.ascii_uppercase
+        elif select_alphabet == "number" or select_alphabet == "Number" or select_alphabet == "NUMBER" or select_alphabet == "n" or select_alphabet == "N":
+            all_char += string.digits
+        elif select_alphabet == "special" or select_alphabet == "Special" or select_alphabet == "SPECIAL" or select_alphabet == "s" or select_alphabet == "S":
+            all_char += string.punctuation
+        count+=1
+    return all_char
 
-print(f"Your password is: {password}")
+def main()-> string:
+    """main function
 
-#TODO: choices about the chars
+    :return: generated password
+    :rtype: string
+    """
+    result = ""
+    i = 0
+    chars = get_chars()
+    pass_length = int(input("Insert the length of your password: "))
+    while i <= pass_length:
+        result += random.choice(chars)
+        i+=1
+    return result
+
+
+if __name__ == "__main__":
+    password = main()
+    print(f"Your password is: {password}")
+
 #TODO: graphic interface
 #TODO: auto copy password
