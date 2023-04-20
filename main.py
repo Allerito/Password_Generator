66c948b37dc1a85f6c0ff537ccb34681a7f291ec
Allerito <allera.ruben.04@gmail.com>
2023-04-20 21:51:15 +0200

    password generator v.1.0.0
    The easiest random password generator possible to do

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+password = ""
+all_char = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+pass_length = int(input("Insert the length of your password: "))
+for i in range(pass_length):
+    password += random.choice(all_char)
+
+print(f"Your password is: {password}")
